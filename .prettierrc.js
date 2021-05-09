65dae0b879434912818d631c463f5a2af8f3b0e6
Aleksandr <alex@enslit.ru>
2021-05-09 22:42:59 +0300

    build: Установить Prettier и ESLint

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+}
