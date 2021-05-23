7753f8b53645e49c708e873866b7384d0cb0cf29
Andrey <ScootalooRainbow@yandex.ru>
2021-05-23 20:42:05 +0900

    mock data

--- src/pages/calendar/CalendarPage.js
+++ src/pages/calendar/CalendarPage.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import CalendarElement from '../../components/Calendar/CalendarElement/CalendarElement';
+
+const testArray = [
+  {
+    day: '05',
+    id: 1,
+    month: 'декабрь',
+    title: 'Субботний митап',
+    time: '14:00 - 15:00',
+    address: 'Садовническая наб., д. 77 стр. 1 (офис компании Ernst&Young)',
+    contacts: 'Александра, +7 926 356-78-90',
+    places: 5,
+  },
+];
 function CalendarPage() {
   return (
     <>
       <h1>Calendar Page</h1>;
-      <CalendarElement />
+      <ul>
+        {testArray.map((item) => (
+          <CalendarElement
+            key={item.id}
+            day={item.day}
+            id={item.id}
+            month={item.month}
+            title={item.title}
+            address={item.address}
+            contacts={item.contacts}
+            places={item.places}
+          />
+        ))}
+      </ul>
     </>
   );
 }
