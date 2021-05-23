06a8163f1fd46ed682616e26929dd4510891c8b8
Andrey <ScootalooRainbow@yandex.ru>
2021-05-23 21:14:48 +0900

    list

--- src/pages/calendar/CalendarPage.js
+++ src/pages/calendar/CalendarPage.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import CalendarElement from '../../components/Calendar/CalendarElement/CalendarElement';
-
+import './calenderPage.css';
 const testArray = [
+  {
+    day: '05',
+    id: 1,
+    month: 'декабрь',
+    title: 'Субботний митап',
+    time: '14:00 - 15:00',
+    address: 'Садовническая наб., д. 77 стр. 1 (офис компании Ernst&Young)',
+    contacts: 'Александра, +7 926 356-78-90',
+    places: 5,
+  },
+  {
+    day: '05',
+    id: 1,
+    month: 'декабрь',
+    title: 'Субботний митап',
+    time: '14:00 - 15:00',
+    address: 'Садовническая наб., д. 77 стр. 1 (офис компании Ernst&Young)',
+    contacts: 'Александра, +7 926 356-78-90',
+    places: 5,
+  },
   {
     day: '05',
     id: 1,
@@ -17,7 +37,7 @@ function CalendarPage() {
   return (
     <>
       <h1>Calendar Page</h1>;
-      <ul>
+      <ul className="calendar__list">
         {testArray.map((item) => (
           <CalendarElement
             key={item.id}
